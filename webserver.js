a2b68c090c6d3de346b0cd3cd94e3339378cae87
mykeyy <mykey.discord@gmail.com>
2025-12-30 15:55:02 +0800

    fix: Move response header for root route to correct position

--- webserver.js
+++ webserver.js
@@ -3,9 +3,8 @@ import http from 'http';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(`
       <!DOCTYPE html>
       <html>
